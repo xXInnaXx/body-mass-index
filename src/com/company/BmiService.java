c9a689204773cf7337705840a42464eb2c203980
innakhabirova <iikhabi@mail.ru>
2022-03-01 19:16:14 +0300

    BmiService.calculate formula was fixed

--- src/com/company/BmiService.java
+++ src/com/company/BmiService.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class BmiService {
     public double calculate(double mass, double high) {
-        return mass / high;
+        return mass / Math.pow(high, 2);
     }
 }
